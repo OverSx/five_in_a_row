5c4b13b0a663c02daa89b6320339663b3952611c
Alexander <glikogen1998@mail.ru>
2019-12-08 14:58:22 +0300

    проверка связи
    лучше это удалить

---
+++ five_in_a_row_game/five_in_a_row_game/I_Am_better_than_you_in_all.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace five_in_a_row_game
+{
+    class I_Am_better_than_you_in_all
+    {
+        //ебать мы жеские
+    }
+}
